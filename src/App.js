fea1eca243cf7a117fa04e482a3c7d944766e3c3
Rostyk Podolets <ros.podolets@gmail.com>
2021-12-16 11:08:43 +0200

    point zero

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import './App.css';
+import Profile from 'components/Profile/Profile';
+import Statistic from 'components/Statistic/Statistic';
+import FriendList from 'components/FriendList/FriendList';
+import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
+import user from './database/user.json';
+import data from './database/data.json';
+import friends from './database/friends.json';
+import transactions from './database/transactions.json';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistic title="Upload stats" stats={data} />
+      <Statistic stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory transactions={transactions} />
+    </>
+  );
 }
 
 export default App;
